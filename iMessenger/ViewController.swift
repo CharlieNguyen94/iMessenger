32166a734f9806bb97867230f7517f0f8d10b10f
CharlieNguyen94 <charlietugnuyen@hotmail.com>
2021-07-28 10:25:16 +0100

    Initial Commit

---
+++ iMessenger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iMessenger
+//
+//  Created by Charlie Nguyen on 28/07/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
