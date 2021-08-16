5c462b06855c627681a9098c338a6b8095d244d1
CharlieNguyen94 <charlietugnuyen@hotmail.com>
2021-08-16 15:46:20 +0100

    Supports dark mode.

--- iMessenger/Controllers/NewConversationViewController.swift
+++ iMessenger/Controllers/NewConversationViewController.swift
@@ -55,7 +55,7 @@ class NewConversationViewController: UIViewController {
         
         searchBar.delegate = self
 
-        view.backgroundColor = .white
+        view.backgroundColor = .systemBackground
         navigationController?.navigationBar.topItem?.titleView = searchBar
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Cancel",
                                                             style: .done,
